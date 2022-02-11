01a3fc74d133c17c58565c43f9f0b833d87da224
Dr-Don <15107787001@sina.cn>
2022-02-11 16:35:28 +0800

    增加多选，拖拽和进度条

--- GiFast/Form1.Designer.cs
+++ GiFast/Form1.Designer.cs
@@ -35,11 +35,16 @@ namespace GiFast
             this.exchangeBtn = new System.Windows.Forms.Button();
             this.jpegMode = new System.Windows.Forms.RadioButton();
             this.pngMode = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.barText1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // fileText
             // 
             resources.ApplyResources(this.fileText, "fileText");
+            this.fileText.ForeColor = System.Drawing.SystemColors.ScrollBar;
             this.fileText.Name = "fileText";
             // 
             // selectBtn
@@ -51,9 +56,11 @@ namespace GiFast
             // 
             // exchangeBtn
             // 
+            this.exchangeBtn.BackColor = System.Drawing.Color.CornflowerBlue;
             resources.ApplyResources(this.exchangeBtn, "exchangeBtn");
+            this.exchangeBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.exchangeBtn.Name = "exchangeBtn";
-            this.exchangeBtn.UseVisualStyleBackColor = true;
+            this.exchangeBtn.UseVisualStyleBackColor = false;
             this.exchangeBtn.Click += new System.EventHandler(this.button2_Click);
             // 
             // jpegMode
@@ -72,19 +79,45 @@ namespace GiFast
             this.pngMode.UseVisualStyleBackColor = true;
             this.pngMode.CheckedChanged += new System.EventHandler(this.pngMode_CheckedChanged);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.barText1);
+            this.groupBox1.Controls.Add(this.exchangeBtn);
+            this.groupBox1.Controls.Add(this.jpegMode);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.pngMode);
+            this.groupBox1.Controls.Add(this.fileText);
+            this.groupBox1.Controls.Add(this.selectBtn);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Gif_DragDrop);
+            this.groupBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Gif_DragEnter);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // barText1
+            // 
+            resources.ApplyResources(this.barText1, "barText1");
+            this.barText1.Name = "barText1";
+            // 
             // GiFast
             // 
+            this.AllowDrop = true;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.pngMode);
-            this.Controls.Add(this.jpegMode);
-            this.Controls.Add(this.exchangeBtn);
-            this.Controls.Add(this.selectBtn);
-            this.Controls.Add(this.fileText);
+            this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
             this.Name = "GiFast";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Gif_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Gif_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,6 +128,9 @@ namespace GiFast
         private System.Windows.Forms.Button exchangeBtn;
         private System.Windows.Forms.RadioButton jpegMode;
         private System.Windows.Forms.RadioButton pngMode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label barText1;
     }
 }
 
--- GiFast/Form1.cs
+++ GiFast/Form1.cs
@@ -14,8 +14,8 @@ namespace GiFast
 {
     public partial class GiFast : Form
     {
-        string sPath = "";
         string sPathOut = "";
+        Dictionary<string, string> srcPath;
         enum Mode
         {
             PNG = 1,
@@ -34,6 +34,14 @@ namespace GiFast
             pngMode.Checked = true;
         }
 
+        void RefreshUI()
+        {
+            srcPath = new Dictionary<string, string>();
+            fileText.Text = "";
+            progressBar1.Value = 0;
+            barText1.Text = "进度：0%";
+        }
+
         // 选择文件
         private void button1_Click(object sender, EventArgs e)
         {
@@ -42,10 +50,17 @@ namespace GiFast
             openFileDialog.Filter = "动图(*.gif)|*.gif";
             openFileDialog.RestoreDirectory = true;
             openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = true;
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                sPath = openFileDialog.FileName;
-                fileText.Text = sPath;
+                RefreshUI();
+                for (int i = 0; i < openFileDialog.FileNames.Length; i++)
+                {
+                    string path = openFileDialog.FileNames[i];
+                    string name = openFileDialog.SafeFileNames[i].Substring(0, openFileDialog.SafeFileNames[i].Length - 4);
+                    srcPath.Add(path, name);
+                    fileText.Text = fileText.Text + path + ";";
+                }
             }
         }
 
@@ -54,7 +69,7 @@ namespace GiFast
         private void button2_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = sPath;
+            saveFileDialog.InitialDirectory = srcPath.First().Key;
             saveFileDialog.Filter = mode == Mode.PNG?"图片(*.png)|*.png":"图片(*.jpeg)|*.jpeg";
             saveFileDialog.FileName = "frame";
             saveFileDialog.RestoreDirectory = true;
@@ -62,24 +77,55 @@ namespace GiFast
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 sPathOut = saveFileDialog.FileName;
-                Image imgGif = Image.FromFile(sPath, true);
-                var ImgFrmDim = new FrameDimension(imgGif.FrameDimensionsList[0]);
-                int count = imgGif.GetFrameCount(ImgFrmDim);
-                for (int i = 0; i < count; i++)
+                int total = srcPath.Count;
+                int success = 0;
+                foreach (KeyValuePair<string, string> itr in srcPath)
                 {
-                    imgGif.SelectActiveFrame(ImgFrmDim, i);
-                    switch(mode)
+                    double progress = (double)success / total;
+                    progressBar1.Value = (int)(progress*100);
+                    barText1.Text = "进度：" + string.Format("{0:N2}", progress);
+                    
+                    Image imgGif = Image.FromFile(itr.Key, true);
+                    var ImgFrmDim = new FrameDimension(imgGif.FrameDimensionsList[0]);
+                    int count = imgGif.GetFrameCount(ImgFrmDim);
+                    int index = sPathOut.LastIndexOf("\\");
+
+                    // 先创建文件夹
+                    string newPath = Path.Combine(sPathOut.Substring(0, index), itr.Value);
+                    if (Directory.Exists(newPath) == false)
                     {
-                        case Mode.PNG:
-                            imgGif.Save(sPathOut.Substring(0, sPathOut.Length - 4) + string.Format(@"{0}.png", i), ImageFormat.Png);
-                            break;
-                        case Mode.JPEG:
-                            imgGif.Save(sPathOut.Substring(0, sPathOut.Length - 5) + string.Format(@"{0}.jpeg", i), ImageFormat.Jpeg);
-                            break;
-                        default:
-                            break;
+                        Directory.CreateDirectory(newPath);
                     }
+
+                    string retPath;
+                    if (Directory.Exists(newPath))
+                    {
+                        retPath = newPath + "\\" + itr.Value;
+                    }
+                    else
+                    {
+                        retPath = sPathOut;
+                        retPath = mode == Mode.PNG ? retPath.Substring(0, retPath.Length - 4) : retPath.Substring(0, retPath.Length - 5);
+                    }
+                    for (int i = 0; i < count; i++)
+                    {
+                        imgGif.SelectActiveFrame(ImgFrmDim, i);
+                        switch(mode)
+                        {
+                            case Mode.PNG:
+                                imgGif.Save( retPath + string.Format(@"_{0}.png", i), ImageFormat.Png);
+                                break;
+                            case Mode.JPEG:
+                                imgGif.Save( retPath + string.Format(@"_{0}.jpeg", i), ImageFormat.Jpeg);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    success += 1;
                 }
+                progressBar1.Value = 100;
+                barText1.Text = "进度：100%";
             }
         }
 
@@ -91,6 +137,34 @@ namespace GiFast
         private void jpegMode_CheckedChanged(object sender, EventArgs e)
         {
             mode = Mode.JPEG;
+        }
+
+        private void Gif_DragDrop(object sender, DragEventArgs e)
+        {
+            RefreshUI();
+            Array data = (System.Array)e.Data.GetData(DataFormats.FileDrop);
+            for (int i = 0; i < data.Length; i++)
+            {
+                string path = data.GetValue(i).ToString();
+                if (path.Substring(path.Length - 4) != ".gif") continue;
+                int index = path.LastIndexOf("\\") + 1;
+                int index2 = path.LastIndexOf(".gif");
+                string name = path.Substring(index, index2 - index);
+                srcPath.Add(path, name);
+                fileText.Text = fileText.Text + path + ";";
+            }
+        }
+
+        private void Gif_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
         }
     }
 }
