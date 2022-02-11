ce383bbce314cd53ab404b7226aba3d88c08a775
Dr-Don <15107787001@sina.cn>
2022-02-11 11:05:44 +0800

    first commit

---
+++ GiFast/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace GiFast
+{
+    partial class GiFast
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GiFast));
+            this.fileText = new System.Windows.Forms.TextBox();
+            this.selectBtn = new System.Windows.Forms.Button();
+            this.exchangeBtn = new System.Windows.Forms.Button();
+            this.jpegMode = new System.Windows.Forms.RadioButton();
+            this.pngMode = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // fileText
+            // 
+            resources.ApplyResources(this.fileText, "fileText");
+            this.fileText.Name = "fileText";
+            // 
+            // selectBtn
+            // 
+            resources.ApplyResources(this.selectBtn, "selectBtn");
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.UseVisualStyleBackColor = true;
+            this.selectBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exchangeBtn
+            // 
+            resources.ApplyResources(this.exchangeBtn, "exchangeBtn");
+            this.exchangeBtn.Name = "exchangeBtn";
+            this.exchangeBtn.UseVisualStyleBackColor = true;
+            this.exchangeBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // jpegMode
+            // 
+            resources.ApplyResources(this.jpegMode, "jpegMode");
+            this.jpegMode.Name = "jpegMode";
+            this.jpegMode.TabStop = true;
+            this.jpegMode.UseVisualStyleBackColor = true;
+            this.jpegMode.CheckedChanged += new System.EventHandler(this.jpegMode_CheckedChanged);
+            // 
+            // pngMode
+            // 
+            resources.ApplyResources(this.pngMode, "pngMode");
+            this.pngMode.Name = "pngMode";
+            this.pngMode.TabStop = true;
+            this.pngMode.UseVisualStyleBackColor = true;
+            this.pngMode.CheckedChanged += new System.EventHandler(this.pngMode_CheckedChanged);
+            // 
+            // GiFast
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pngMode);
+            this.Controls.Add(this.jpegMode);
+            this.Controls.Add(this.exchangeBtn);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.fileText);
+            this.MaximizeBox = false;
+            this.Name = "GiFast";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileText;
+        private System.Windows.Forms.Button selectBtn;
+        private System.Windows.Forms.Button exchangeBtn;
+        private System.Windows.Forms.RadioButton jpegMode;
+        private System.Windows.Forms.RadioButton pngMode;
+    }
+}
+
---
+++ GiFast/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace GiFast
+{
+    public partial class GiFast : Form
+    {
+        string sPath = "";
+        string sPathOut = "";
+        enum Mode
+        {
+            PNG = 1,
+            JPEG = 2
+        }
+        Mode mode = Mode.PNG;
+
+        public GiFast()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            pngMode.Checked = true;
+        }
+
+        // 选择文件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "动图(*.gif)|*.gif";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sPath = openFileDialog.FileName;
+                fileText.Text = sPath;
+            }
+        }
+
+
+        // 转换
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = sPath;
+            saveFileDialog.Filter = mode == Mode.PNG?"图片(*.png)|*.png":"图片(*.jpeg)|*.jpeg";
+            saveFileDialog.FileName = "frame";
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.FilterIndex = 1;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                sPathOut = saveFileDialog.FileName;
+                Image imgGif = Image.FromFile(sPath, true);
+                var ImgFrmDim = new FrameDimension(imgGif.FrameDimensionsList[0]);
+                int count = imgGif.GetFrameCount(ImgFrmDim);
+                for (int i = 0; i < count; i++)
+                {
+                    imgGif.SelectActiveFrame(ImgFrmDim, i);
+                    switch(mode)
+                    {
+                        case Mode.PNG:
+                            imgGif.Save(sPathOut.Substring(0, sPathOut.Length - 4) + string.Format(@"{0}.png", i), ImageFormat.Png);
+                            break;
+                        case Mode.JPEG:
+                            imgGif.Save(sPathOut.Substring(0, sPathOut.Length - 5) + string.Format(@"{0}.jpeg", i), ImageFormat.Jpeg);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void pngMode_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = Mode.PNG;
+        }
+
+        private void jpegMode_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = Mode.JPEG;
+        }
+    }
+}
